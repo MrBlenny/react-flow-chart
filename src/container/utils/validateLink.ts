a110c5057322433c577b4215d5a6c1942cd90240
sipfoundry <martin@caliviaconsult.com>
2019-06-12 09:56:11 +0200

    remove console.log

--- src/container/utils/validateLink.ts
+++ src/container/utils/validateLink.ts
@@ -3,7 +3,7 @@ import {
 } from '../../'
 
 export const validateLink = ({ linkId, fromNodeId, fromPortId, toNodeId, toPortId }: IOnLinkCompleteInput, chart: IChart): boolean => {
-  console.log('validateLink: ', linkId, fromNodeId, fromPortId, toNodeId, toPortId)
+  // no links between same type nodes
   if (chart.nodes[fromNodeId].type === chart.nodes[toNodeId].type) return false
   return true
 }
