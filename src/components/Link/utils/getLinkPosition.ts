f55ad66001656f033ee9f0682cff0cc18ba801f2
Jeffrey Hunter <sublet@me.com>
2019-12-20 10:37:13 -0500

    Adding some error reporting

--- src/components/Link/utils/getLinkPosition.ts
+++ src/components/Link/utils/getLinkPosition.ts
@@ -2,8 +2,22 @@ import { INode, IPosition } from '../../../'
 
 export const getLinkPosition = (node: INode, portId: string): IPosition => {
   const port = node.ports[portId]
+  if (port) {
+    return {
+      x: node.position.x + (port.position ? port.position.x : 0),
+      y: node.position.y + (port.position ? port.position.y : 0)
+    }
+  }
+  console.error(`Can't find ${portId} on Node ${node.id}`)
+  console.log(node)
+  if (node && node.position) {
+    return {
+      x: node.position.x,
+      y: node.position.y
+    }
+  }
   return {
-    x: node.position.x + (port.position ? port.position.x : 0),
-    y: node.position.y + (port.position ? port.position.y : 0),
+    x: 0,
+    y: 0
   }
 }
