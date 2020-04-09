4234228f69f1fedb5ed53f0d210781deaaa419aa
Furkan Akyol <furkanakyol1997@gmail.com>
2020-04-09 21:36:26 +0300

    onNodeClick, onLinkClick callbacks implemented for FlowChartWithState, default callbacks can be overriden with callbacks prop so that annoying onDeleteKey callback can be changed or dismissed

--- src/container/FlowChartWithState.tsx
+++ src/container/FlowChartWithState.tsx
@@ -7,6 +7,9 @@ export interface IFlowChartWithStateProps {
   initialValue: IChart
   Components?: IFlowChartComponents
   config?: IConfig
+  callbacks?: any
+  onNodeClick?: any
+  onLinkClick?: any
 }
 
 /**
@@ -21,13 +24,27 @@ export class FlowChartWithState extends React.Component<IFlowChartWithStateProps
     super(props)
     this.state = props.initialValue
   }
+  
+  
+  public static defaultProps = {
+    callbacks: {},
+    onNodeClick:()=>{return},
+    onLinkClick:()=>{return}
+  };
+  
+  
   public render () {
     const { Components, config } = this.props
 
     return (
       <FlowChart
         chart={this.state}
-        callbacks={this.stateActions}
+        callbacks={{
+          ...this.stateActions, 
+          onNodeClick:(nodeId)=>{this.props.onNodeClick(nodeId); return this.stateActions.onNodeClick(nodeId)}, onDeleteKey:(x)=>console.log(x),
+          onLinkClick:(linkId)=>{this.props.onNodeClick(linkId); return this.stateActions.onLinkClick(linkId)},
+          ...this.props.callbacks
+        }} //...this.props.callbacks 
         Components={Components}
         config={config}
       />
