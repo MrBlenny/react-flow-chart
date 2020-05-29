563f5c8667f16e57383fd65250329a6aca311725
Andrew <andydrew313@gmail.com>
2020-05-28 20:31:15 -0700

    make definitions generic for type-safe components

--- src/types/chart.ts
+++ src/types/chart.ts
@@ -1,59 +1,64 @@
-import { IPosition, ISize } from './generics'
+import { IPosition, ISize } from "./generics";
 
-export interface IChart {
-  offset: IPosition
+export interface IChart<
+  ChartProps = any,
+  NodeProps = any,
+  LinkProps = any,
+  PortProps = any
+> {
+  offset: IPosition;
   nodes: {
-    [id: string]: INode,
-  }
+    [id: string]: INode<NodeProps, PortProps>;
+  };
   links: {
-    [id: string]: ILink,
-  }
-  scale: number
-  properties?: any
+    [id: string]: ILink<LinkProps>;
+  };
+  scale: number;
+  properties?: ChartProps;
 
   /** System Temp */
-  selected: ISelectedOrHovered
-  hovered: ISelectedOrHovered
+  selected: ISelectedOrHovered;
+  hovered: ISelectedOrHovered;
 }
 
 export interface ISelectedOrHovered {
-  type?: 'link' | 'node' | 'port'
-  id?: string
+  type?: "link" | "node" | "port";
+  id?: string;
 }
 
-export interface INode {
-  id: string
-  type: string
-  position: IPosition
-  orientation?: number
+export interface INode<NodeProps = any, PortProps = any> {
+  id: string;
+  type: string;
+  position: IPosition;
+  orientation?: number;
   ports: {
-    [id: string]: IPort,
-  }
-  properties?: any
+    [id: string]: IPort<PortProps>;
+  };
+  properties?: NodeProps;
   /** System Temp */
-  size?: ISize
+  size?: ISize;
 }
 
-export interface IPort {
-  id: string
-  type: string
-  value?: string
-  properties?: any
+export interface IPort<W = any> {
+  id: string;
+  type: string;
+  value?: string;
+  properties?: W;
   /** System Temp */
-  position?: IPosition
+  position?: IPosition;
 }
 
-export interface ILink {
-  id: string
+export interface ILink<V = any> {
+  id: string;
   from: {
-    nodeId: string
-    portId: string,
-  }
+    nodeId: string;
+    portId: string;
+  };
   to: {
-    nodeId?: string
-    portId?: string
+    nodeId?: string;
+    portId?: string;
     /** System Temp */
-    position?: IPosition,
-  }
-  properties?: any
+    position?: IPosition;
+  };
+  properties?: V;
 }
--- stories/misc/exampleChartState.ts
+++ stories/misc/exampleChartState.ts
@@ -1,6 +1,11 @@
-import { IChart } from '../../src'
+import { IChart } from "../../src";
 
-export const chartSimple: IChart = {
+export const chartSimple: IChart<
+  any,
+  { test: number },
+  { label: string },
+  { value: string }
+> = {
   offset: {
     x: 0,
     y: 0,
@@ -8,125 +13,137 @@ export const chartSimple: IChart = {
   scale: 1,
   nodes: {
     node1: {
-      id: 'node1',
-      type: 'output-only',
+      id: "node1",
+      type: "output-only",
       position: {
         x: 300,
         y: 100,
       },
+      properties: {
+        test: 0,
+      },
       ports: {
         port1: {
-          id: 'port1',
-          type: 'output',
+          id: "port1",
+          type: "output",
           properties: {
-            value: 'yes',
+            value: "yes",
           },
         },
         port2: {
-          id: 'port2',
-          type: 'output',
+          id: "port2",
+          type: "output",
           properties: {
-            value: 'no',
+            value: "no",
           },
         },
       },
     },
     node2: {
-      id: 'node2',
-      type: 'input-output',
+      id: "node2",
+      type: "input-output",
       position: {
         x: 300,
         y: 300,
       },
+      properties: {
+        test: 1,
+      },
       ports: {
         port1: {
-          id: 'port1',
-          type: 'input',
+          id: "port1",
+          type: "input",
         },
         port2: {
-          id: 'port2',
-          type: 'output',
+          id: "port2",
+          type: "output",
         },
       },
     },
     node3: {
-      id: 'node3',
-      type: 'input-output',
+      id: "node3",
+      type: "input-output",
       position: {
         x: 100,
         y: 600,
       },
+      properties: {
+        test: 2,
+      },
       ports: {
         port1: {
-          id: 'port1',
-          type: 'input',
+          id: "port1",
+          type: "input",
         },
         port2: {
-          id: 'port2',
-          type: 'output',
+          id: "port2",
+          type: "output",
         },
       },
     },
     node4: {
-      id: 'node4',
-      type: 'input-output',
+      id: "node4",
+      type: "input-output",
       position: {
         x: 500,
         y: 600,
       },
+      properties: {
+        test: 3,
+      },
       ports: {
         port1: {
-          id: 'port1',
-          type: 'input',
+          id: "port1",
+          type: "input",
         },
         port2: {
-          id: 'port2',
-          type: 'output',
+          id: "port2",
+          type: "output",
         },
       },
     },
   },
   links: {
     link1: {
-      id: 'link1',
+      id: "link1",
       from: {
-        nodeId: 'node1',
-        portId: 'port2',
+        nodeId: "node1",
+        portId: "port2",
       },
       to: {
-        nodeId: 'node2',
-        portId: 'port1',
+        nodeId: "node2",
+        portId: "port1",
       },
       properties: {
-        label: 'example link label',
+        label: "example link label",
       },
     },
     link2: {
-      id: 'link2',
+      id: "link2",
       from: {
-        nodeId: 'node2',
-        portId: 'port2',
+        nodeId: "node2",
+        portId: "port2",
       },
       to: {
-        nodeId: 'node3',
-        portId: 'port1',
+        nodeId: "node3",
+        portId: "port1",
       },
       properties: {
-        label: 'another example link label',
+        label: "another example link label",
       },
     },
     link3: {
-      id: 'link3',
+      id: "link3",
       from: {
-        nodeId: 'node2',
-        portId: 'port2',
+        nodeId: "node2",
+        portId: "port2",
       },
       to: {
-        nodeId: 'node4',
-        portId: 'port1',
+        nodeId: "node4",
+        portId: "port1",
       },
     },
   },
   selected: {},
   hovered: {},
-}
+};
